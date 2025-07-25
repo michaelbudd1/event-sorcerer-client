056ac4d7ccc7aec28c341e5b7338155252a8f82c
michaelbudd1 <michaelbudd6@gmail.com>
2025-07-25 21:59:29 +0100

    wip

--- src/Infrastructure/Client.php
+++ src/Infrastructure/Client.php
@@ -68,7 +68,11 @@ final readonly class Client
                 );
 
                 $connection->on('data', function (string $events) use ($applicationId, $connection, $eventHandler)  {
-                    $this->addEventsForProcessing($applicationId, $events);
+                    foreach (\array_filter(explode(MessageMarkup::NewEventParser->value, $events)) as $event) {
+                        $eventHandler(self::decodeEvent($event));
+                    }
+
+//                    $this->addEventsForProcessing($applicationId, $events);
 
 //                    while ($decodedEvent = $this->availableEvents->fetchOne($applicationId)) {
 //                        $streamId = StreamId::fromString($decodedEvent['stream']);
