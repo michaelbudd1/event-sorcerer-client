e09a81c319c5eddcc0bc87304d70a414e164a7dd
michaelbudd1 <michaelbudd6@gmail.com>
2025-11-03 20:56:14 +0000

    wip

--- src/Infrastructure/Client.php
+++ src/Infrastructure/Client.php
@@ -222,7 +222,7 @@ final readonly class Client
         Checkpoint $streamCheckpoint,
         Checkpoint $allStreamCheckpoint
     ): void {
-        (new Connector())->connect(self::IPC_URI)->then(
+        (new Connector(loop: Loop::get()))->connect(self::IPC_URI)->then(
             function (ConnectionInterface $connection) use ($stream, $streamCheckpoint, $allStreamCheckpoint) {
                 $connection->write(
                     CreateMessage::forAcknowledgement(
