284cea28d4feb24db79715397dfc0caa5188d4f7
michaelbudd1 <michaelbudd6@gmail.com>
2025-11-04 08:34:05 +0000

    wip

--- src/Infrastructure/Client.php
+++ src/Infrastructure/Client.php
@@ -222,7 +222,9 @@ final readonly class Client
         Checkpoint $streamCheckpoint,
         Checkpoint $allStreamCheckpoint
     ): void {
-        (new Connector(loop: Loop::get()))->connect(self::IPC_URI)->then(
+        $loop = Loop::get();
+
+        (new Connector(loop: $loop))->connect(self::IPC_URI)->then(
             function (ConnectionInterface $connection) use ($stream, $streamCheckpoint, $allStreamCheckpoint) {
                 echo 'Yes I\'m writing an acknowledgement!' . PHP_EOL;
 
@@ -239,6 +241,8 @@ final readonly class Client
                 $connection->close();
             }
         );
+
+        $loop?->run();
     }
 
     public function writeNewEvent(
